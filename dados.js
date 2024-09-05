2d2bc839d733e7ee94093c81424ba75343e08703
Edson José Lopes Jr. <86385232+Edinho-02@users.noreply.github.com>
2024-09-05 15:37:29 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+  {
+    titulo: "Rayssa Leal",
+    descricao:
+      "Rayssa, filha de Lilian e Haroldo Leal, nasceu e mora em Imperatriz, segunda maior cidade do Maranhão, e alterna os estudos escolares com os treinamentos. Começou a treinar o esporte aos seis anos de idade, após receber um skate de aniversário de um amigo de seu pai. Apesar de não se importar de ser chamada de “Fadinha”, a atleta prefere ser chamada de Rayssa Leal. Ela realiza psicoterapia e recebe acompanhamento dos familiares e da empresária para direcioná-la na carreira. Multi campeã em vários campeonatos de skate.",
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+  },
+  {
+    titulo: "Rebeca Andrade",
+    descricao:
+      "Rebeca Andrade é um nome que brilha no cenário esportivo brasileiro. Com sua força, agilidade e determinação, ela conquistou o coração dos brasileiros e se tornou um ícone da ginástica artística.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+  },
+  {
+    titulo: "Beatriz Souza",
+    descricao:
+      "Bia Souza, nascida em 20 de maio de 1998, na cidade paulista de Itariri, Beatriz é a filha mais nova do militar aposentado do Corpo de Bombeiros de São Paulo e ex-judoca brasileiro Poscedonio José de Souza Neto, mas foi criada na cidade de Peruíbe. Além de atleta, é sargento do exército brasileiro. Casou-se com o ex-jogador de basquete Daniel Souza. Foi a primeira brasileira a ganhar a medalha de ouro nas Olimpíadas Paris 2024.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+  },
+];
